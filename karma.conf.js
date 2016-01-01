87f67776c43022794620e09bbb2bdc070fed3bf6
Nick Saunders <nick@saunde.rs>
2016-01-01 13:57:37 -0700

    Karma config formatting

--- karma.conf.js
+++ karma.conf.js
@@ -1,47 +1,32 @@
 module.exports = function(config) {
-  config.set({
-
-    basePath: '',
-
-    frameworks: ['jasmine'],
-
-    files: [
-      // paths loaded by Karma
-      'node_modules/es6-module-loader/dist/es6-module-loader.js',
-      'node_modules/es6-shim/es6-shim.js',
-      'node_modules/angular2/bundles/angular2-polyfills.js',
-      'node_modules/systemjs/dist/system.src.js',
-      'node_modules/rxjs/bundles/Rx.js',
-      'node_modules/angular2/bundles/angular2.js',
-      'node_modules/angular2/bundles/testing.dev.js',
-      'karma-test-shim.js',
-      {pattern: 'src/test/matchers.js', included: true, watched: true},
-
-      // paths loaded via module imports
-      {pattern: 'src/**/*.js', included: false, watched: true},
-
-      // paths loaded via Angular's component compiler
-      // (these paths need to be rewritten, see proxies section)
-      {pattern: 'src/**/*.html', included: false, watched: true},
-      {pattern: 'src/**/*.css', included: false, watched: true},
-
-      // paths to support debugging with source maps in dev tools
-      {pattern: 'src/**/*.ts', included: false, watched: false},
-      {pattern: 'src/**/*.js.map', included: false, watched: false}
-    ],
-
-    // proxied base paths
-    proxies: {
-      // required for component assests fetched by Angular's compiler
-      "/app/": "/base/src/app/"
-    },
-
-    reporters: ['progress'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-  })
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            'node_modules/es6-module-loader/dist/es6-module-loader.js',
+            'node_modules/es6-shim/es6-shim.js',
+            'node_modules/angular2/bundles/angular2-polyfills.js',
+            'node_modules/systemjs/dist/system.src.js',
+            'node_modules/rxjs/bundles/Rx.js',
+            'node_modules/angular2/bundles/angular2.js',
+            'node_modules/angular2/bundles/testing.dev.js',
+            'karma-test-shim.js',
+            { pattern: 'src/test/matchers.js', included: true, watched: true },
+            { pattern: 'src/**/*.js', included: false, watched: true },
+            { pattern: 'src/**/*.html', included: false, watched: true },
+            { pattern: 'src/**/*.css', included: false, watched: true },
+            { pattern: 'src/**/*.ts', included: false, watched: false },
+            { pattern: 'src/**/*.js.map', included: false, watched: false }
+        ],
+        proxies: {
+            "/app/": "/base/src/app/"
+        },
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: false
+    });
 }
